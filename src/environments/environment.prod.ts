bd0e4f3fd68bbf5ec55738ec38682878072e0bb5
Adrian Popa <adipopa9910@gmail.com>
2018-04-18 23:53:34 +0300

    Added environment variables, configured paths

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  baseURL: 'https://amber-net-api.herokuapp.com',
+  notificationQueue: 'notificationsPublisher',
+  toasts: {
+    duration: 10000
+  }
+};
